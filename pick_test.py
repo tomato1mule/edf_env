e6a4a4b9705f058de1cce96c599ce893c65e32c0
Hyunwoo Ryu <tomato1mule@gmail.com>
2023-01-28 22:21:12 +0900

    pick test done

--- pick_test.py
+++ pick_test.py
@@ -4,6 +4,11 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 
+import numpy as np
+
+
+
+
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
 
@@ -12,6 +17,8 @@ scene = moveit_commander.PlanningSceneInterface()
 
 group_name = "arm"
 move_group = moveit_commander.MoveGroupCommander(group_name)
+gripper_group_name = "gripper"
+gripper_group = moveit_commander.MoveGroupCommander(gripper_group_name)
 
 move_group.set_pose_reference_frame('map')
 
@@ -40,9 +47,34 @@ pose_goal.orientation.x = 0.0
 pose_goal.orientation.y = 1.0
 pose_goal.orientation.z = 0.0
 pose_goal.orientation.w = 0.0
-pose_goal.position.x = 0.0
+pose_goal.position.x = -0.25
 pose_goal.position.y = 0.0
-pose_goal.position.z = 0.75
+pose_goal.position.z = 0.62
+
+move_group.set_pose_target(pose_goal)
+
+success = move_group.go(wait=True)
+move_group.stop()
+move_group.clear_pose_targets()
+
+
+
+
+joint_goal = gripper_group.get_current_joint_values()
+joint_goal[0] = 0.7
+gripper_group.go(joint_goal, wait=True)
+gripper_group.stop()
+
+
+theta = np.pi / 6
+pose_goal = geometry_msgs.msg.Pose()
+pose_goal.orientation.x = 0.0
+pose_goal.orientation.y = np.sin(theta/2)
+pose_goal.orientation.z = 0.0
+pose_goal.orientation.w = np.cos(theta/2)
+pose_goal.position.x = -0.25
+pose_goal.position.y = 0.0
+pose_goal.position.z = 0.8
 
 move_group.set_pose_target(pose_goal)
 
